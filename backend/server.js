6f6444569605c0b814db86045e01df27362d5253
Himanshu Singh <121339915+HimanshuGitCode@users.noreply.github.com>
2025-04-13 13:12:54 +0530

    commit all

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
+import cartRouter from './routes/cartRoute.js'
+import orderRouter from './routes/orderRoute.js'
+
+// App Config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(cors({
+  origin: [
+    process.env.FRONTEND_URL,
+    process.env.ADMIN_URL,
+    'http://localhost:5173', // Frontend dev server
+    'http://localhost:5174'  // Admin dev server
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
+// api endpoints
+app.use('/api/user',userRouter)
+app.use('/api/product',productRouter)
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
+
+app.get('/',(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log('Server started on PORT : '+ port))
